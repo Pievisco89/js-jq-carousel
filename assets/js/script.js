d62ce9d2882042202483d670914066878026cb64
Pievisco89 <pie-89@live.it>
2021-04-20 18:09:43 +0200

    fine manca il bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,51 @@
-console.log("ciaoooooo")
+$(document).ready(function(){
+
+  $('.next').click(nextImage);
+  $('.prev').click(prevImage);
+
+  function nextImage(){
+    /* referenze */
+    var activeImg = $('.box-img img.active');
+    var activeCircle = $('.circle i.active');
+    /* tolgo classe active */
+    activeImg.removeClass('active');
+    activeCircle.removeClass('active');
+    
+    if(activeImg.hasClass('last')){
+    /* dopo l'ultima img active passa alla prima */  
+      $('.box-img img.first').addClass('active');
+      $('.circle i.first').addClass('active');
+
+    }else {
+      /* active passa ad ogni img successiva */
+      activeImg.next('img').addClass('active');
+      activeCircle.next('i').addClass('active');
+
+    }
+    
+
+  }
+
+  function prevImage(){
+    /* referenze */
+    var activeImg = $('.box-img img.active');
+    var activeCircle = $('.circle i.active');
+    /* tolgo classe active */
+    activeImg.removeClass('active');
+    activeCircle.removeClass('active');
+    /*  */
+    if(activeImg.hasClass('first')){
+      /* se arrivo alla prima, active passa all'ultima */
+      $('.box-img img.last').addClass('active');
+      $('.circle i.last').addClass('active');
+
+    }else{
+      /* active passa ad ogni img precedente */
+      activeImg.prev('img').addClass('active');
+      activeCircle.prev('i').addClass('active');
+
+    }
+
+  }
+
+})
